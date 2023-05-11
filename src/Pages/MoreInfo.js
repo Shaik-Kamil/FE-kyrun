340ac72ba12b23235914d74e6be2caee02c6b033
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-05-11 00:33:21 -0400

    created user stories && more info pge

---
+++ src/Pages/MoreInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MoreInfo(props) {
+    return (
+        <div>
+            <h1>Kyrun</h1>
+            <p>Kyrun is an app that connects you with local joggers in your area.</p>
+            <h2>How it Works</h2>
+            <p>With Kyrun, you can filter runners based on gender, age, location, and even running pace. Once you've selected your preferences, you can browse through the results and click on the profiles that share your common interests and start building your netwrok.</p>
+            <p>One key feature of Kyrun is the SOS button, which you can use in case of an imminent threat during a jog with the person you matched with.</p>
+            <p>In addition to matching with other joggers who share similar interests, Kyrun also offers a convenient chat feature that allows users to communicate directly with their matched profiles. This way, users can easily plan jogging sessions, ask questions, and get to know their jogging partners better before they meet in person.</p>
+            <p>Build your network today!</p>
+      </div>
+    );
+}
+
+export default MoreInfo;
