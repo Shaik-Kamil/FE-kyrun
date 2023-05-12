9cf5117a33341014400cbd4afd0ba2ad8103e156
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-05-12 10:22:30 -0400

    updated make profile and more info

--- src/Pages/MoreInfo.js
+++ src/Pages/MoreInfo.js
@@ -3,13 +3,29 @@ import React from 'react';
 function MoreInfo(props) {
     return (
         <div>
-            <h1>Kyrun</h1>
-            <p>Kyrun is an app that connects you with local joggers in your area.</p>
-            <h2>How it Works</h2>
-            <p>With Kyrun, you can filter runners based on gender, age, location, and even running pace. Once you've selected your preferences, you can browse through the results and click on the profiles that share your common interests and start building your netwrok.</p>
-            <p>One key feature of Kyrun is the SOS button, which you can use in case of an imminent threat during a jog with the person you matched with.</p>
-            <p>In addition to matching with other joggers who share similar interests, Kyrun also offers a convenient chat feature that allows users to communicate directly with their matched profiles. This way, users can easily plan jogging sessions, ask questions, and get to know their jogging partners better before they meet in person.</p>
+            <section>
+                <h1>Kyrun</h1>
+                <p>Connect with local joggers in your area.</p>
+                {/* add image */}
+            </section>
+            <section>
+                <h2>Find your match made in running heaven with our custom filters</h2>
+                <p>With Kyrun, you can filter runners based on gender, age, location, and even running pace. Once you've selected your preferences, you can browse through the results and click on the profiles that share your common interests and start building your netwrok.</p>
+                {/* add image that demonstrates a filter */}
+            </section>
+            <section>
+                <h2>Tie up your laces and start chatting with your running buddy</h2>
+                <p>Easily plan jogging sessions, ask questions, and get to know your jogging partners better with our in App chat feature.</p>
+                {/* add image */}
+            </section>
+            <section>
+                <h2>On Extra Step of Security</h2>
+                <p>One key feature of Kyrun is the SOS button, which you can use in case of an imminent threat during a jog with the person you matched with.</p>
+                {/* add image */}
+            </section>
+            
             <p>Build your network today!</p>
+
       </div>
     );
 }
