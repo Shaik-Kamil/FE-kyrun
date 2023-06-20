51cb07bffd11d1d7b6e3ad44990108c63f15071f
melanieinpursuit <melaniegonzalez@pursuit.org>
2023-06-19 22:49:04 -0400

    about pages done

--- src/Components/Melanie.js
+++ src/Components/Melanie.js
@@ -1,37 +1,68 @@
 import React from 'react';
-// import '../components/About.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-function Melanie() {
+
+function Melanie () {
   return (
-    <main>
-      <section className="melanie">
-        <div id="aboutSection">
-          <h2 id="Melanie">Melanie Gonzalez</h2>
-          <img
-            alt="Melanie Gonzalez"
-            id="abouts"
-            src="https://avatars.githubusercontent.com/u/107489396?v=4"
-          />
-          <p>bio here</p>
-          <p>another paragraph here if necessary</p>
+    <main style={{marginTop: '100px'}}>
+      <section className="bg-dark py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-12 order-lg-1 my-lg-5">
+              <div className="card border-dark shadow">
+                <div className="image-container">
+                  <img
+                    src="https://avatars.githubusercontent.com/u/107489396?v=4"
+                    alt="Melanie"
+                    className="card-img-top img-fluid shadow-lg animate__animated animate__pulse bw-img"
+                    style={{ border: 'none' }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-8 col-md-12 order-lg-2">
+              <div className="card bg-light border-dark shadow p-4 mt-4">
+                <h2 className="mb-4">Melanie Gonzalez</h2>
+                <p>
+                    <p>I am an upcoming Full Stack Web Developer currently training as a Pursuit Fellow. My current goal is to become a developer for a video game company and help create incredible gameplay as well as create new, relatable and diverse stories for those in marginalized communities, such as the LGBTQ+ community, women, women of color, disabled people, and so many others who don't have much representation in the gaming industry. </p>
+
+                    <p>My extensive knowledge and passion regarding video games and my life experience as an Afro-Latina woman who's a part of the LGBTQ+ community in the gaming industry makes it easy for me to see what is lacking in said industry, and that is what I intend to focus on once I have garnered the skills from my training at Pursuit.</p>
+
+                    <p>I plan to use JavaScript as a foundation and continue learning programming languages to aid game developers create inclusive gaming experiences while also not alienating the core gaming community. My main goal is to create games like the Dark Souls series, The Witcher, and other RPGs with more inclusive aspects such as including traditionally black hairstyles in the character creation setting, implementing storylines with LGBTQ+ elements, adding disabled characters without stigmatizing their disabilities, among other things.</p>
+                </p>
+                <div className="mt-4">
+                  <Link to="https://github.com/melanieinpursuit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-dark mr-4"
+                    style={{margin: '10px'}}
+                  >
+                    <FontAwesomeIcon icon={faGithub} className="mr-2" />
+                    GitHub
+                  </Link>
+                  <span className="ml-4">
+                    <Link to="https://www.linkedin.com/in/melaniegonzalez7"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-dark"
+                    >
+                      <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
+                      LinkedIn
+                    </Link>
+                  </span>
+                </div>
+              </div>
+                  <div className='back'>
+                      <Link to={`/about`}>
+                        <button className='borderman btn-border' style={{ border: 'none', outline: 'none', padding: '10px', backgroundColor: '#F18701', borderRadius: '5px', width: '200px', height: '50px', fontSize: '20px', color: '#FFFFFF', margin: '10px' }}>Back</button>
+                      </Link>
+                    </div>
+            </div>
+          </div>
         </div>
       </section>
-      <div>
-        <li className="return">
-          {' '}
-          <Link to="/About">Go Back</Link>
-        </li>
-      </div>
-      <footer className="footer">
-        Connect with Us!
-        <a href="https://github.com/Yihttps://github.com/melanieinpursuitannar">
-          Melanie's GitHub
-        </a>
-        <a href="https://www.linkedin.com/in/melaniegonzalez7/">
-          Melanie's linkedin
-        </a>
-      </footer>
     </main>
   );
 }
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../CSS/Melanie.css'
 
 export default function AboutUs() {
   return (
-    <div id="aboutSection">
+    <div id="aboutSection" class="feature bg-primary bg-gradient text-white rounded-3 mb-3" style={{ marginTop: '130px', width: '100%', paddingTop: '30px'}}>
       <h1>About Kyrun</h1>
       <p
         style={
@@ -14,25 +15,66 @@ export default function AboutUs() {
           }
         }
       >
-        Kyrun was developed as a group effort by a team of five members.We hope
+        Kyrun was developed as a group effort by a team of five members. We hope
         you enjoy what we have made so far.
       </p>
       <br />
-      <button>
-        <Link to="/Shaik">Shaik</Link>
+      <button style={{margin: '20px'}}>
+      <Link to="/Shaik">
+            <div class="card border-left-primary shadow h-100 py-2" >
+                <div class="card-body" style={{ textAlign: 'center', textDecoration: 'none' }}>
+                  <img alt="Shaik Kamil" id="abouts" src="https://avatars.githubusercontent.com/u/107490051?v=4" style={{width: '200px', height: '200px'}}/>
+                      <h3>Shaik Kamil</h3>
+                </div>
+            </div>
+        </Link>
       </button>
-      <button>
-        <Link to="/Richie">Richie</Link>
+      <button style={{margin: '20px'}}>
+        <Link to="/Richie">
+            <div class="card border-left-primary shadow h-100 py-2" >
+                <div class="card-body" style={{ textAlign: 'center', textDecoration: 'none' }}>
+                  <img alt="Richard Carrasco" id="abouts" src="https://avatars.githubusercontent.com/u/107490202?v=4" style={{width: '200px', height: '200px'}}/>
+                      <h3>Richard Carrasco</h3>
+                </div>
+            </div>
+        </Link>
       </button>
-      <button>
-        <Link to="/Melanie">Melanie</Link>
+      <button style={{margin: '20px'}}>
+        <Link to="/Melanie">
+          <div class="card border-left-primary shadow h-100 py-2" >
+              <div class="card-body" style={{ textAlign: 'center', textDecoration: 'none' }}>
+                <img alt="Melanie Gonzalez" id="abouts" src="https://avatars.githubusercontent.com/u/107489396?v=4" style={{width: '200px', height: '200px'}}/>
+                      <h3>Melanie Gonzalez</h3>
+                      {/* <p class="card-title" style={{ marginTop: '20px', color: '#F18701'}}>{group.title}</p> */}
+              </div>
+          </div>
+        </Link>
       </button>
-      <button>
-        <Link to="/Yianna">Yianna</Link>
+      <button style={{margin: '20px'}}>
+        <Link to="/Yianna">
+          <div class="card border-left-primary shadow h-100 py-2" >
+              <div class="card-body" style={{ textAlign: 'center', textDecoration: 'none' }}>
+                <img alt="Yianna Richey" id="abouts" src="https://avatars.githubusercontent.com/u/107489608?v=4" style={{width: '200px', height: '200px'}}/>
+                      <h3>Yianna Richey</h3>
+              </div>
+          </div>
+        </Link>
       </button>
-      <button>
-        <Link to="/Becky">Becky</Link>
+      <button style={{margin: '20px'}}>
+        <Link to="/Becky">
+            <div class="card border-left-primary shadow h-100 py-2" >
+                <div class="card-body" style={{ textAlign: 'center', textDecoration: 'none' }}>
+                  <img alt="Rebecca Garcia" id="abouts" src="https://avatars.githubusercontent.com/u/22225723?v=4" style={{width: '200px', height: '200px'}}/>
+                      <h3>Rebecca Garcia</h3>
+                </div>
+            </div>
+          </Link>
       </button>
+      <div className='back'>
+                      <Link to={`/`}>
+                        <button className='borderman btn-border' style={{ border: 'none', outline: 'none', padding: '10px', backgroundColor: '#F18701', borderRadius: '5px', width: '200px', height: '50px', fontSize: '20px', color: '#FFFFFF', margin: '10px' }}>Back</button>
+                      </Link>
+                    </div>
     </div>
   );
 }
